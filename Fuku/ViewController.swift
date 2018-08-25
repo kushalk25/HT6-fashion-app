3dc81a7db072d650fb5c18d897fd7455f68cd069
Kushal Kumarasinghe <kushal.k@hotmail.com>
2018-08-24 22:21:46 -0400

    Initial Commit

---
+++ Fuku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fuku
+//
+//  Created by Kushal Kumarasinghe on 2018-08-24.
+//  Copyright © 2018 Kushal Kumarasinghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
