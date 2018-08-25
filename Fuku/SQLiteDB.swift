0a46f8e0ed251dc775560aeb880ab71f0ec57199
Kushal Kumarasinghe <kushal.k@hotmail.com>
2018-08-25 01:52:36 -0400

    added clothing table functionality

--- Fuku/SQLiteDB.swift
+++ Fuku/SQLiteDB.swift
@@ -14,6 +14,15 @@ class SQLiteDB {
     static let instance = SQLiteDB()
     private let db: Connection?
     
+    // Clothing Table
+    private let clothingTable = Table("clothing")
+    private let clothingId = Expression<Int>("id")
+    private let clothingName = Expression<String>("name")
+    private let clothingType = Expression<String>("type")
+    private let clothingColour = Expression<String>("colour")
+    
+    // flags
+    private let deleteClothingTable = false
     
 
     private init(){
@@ -26,12 +35,53 @@ class SQLiteDB {
         do {
             db = try Connection("\(path)/db.sqlite3")
             print("db found")
-            print(path)
+            createClothingTable()
         } catch {
             db = nil
             print("unable to find db")
         }
             
+    }
+    
+    
+    private func createClothingTable() {
+    
+        do {
+            if (deleteClothingTable) {
+                try db?.run(clothingTable.drop(ifExists: true))
+            }
+            
+            try db!.run(clothingTable.create(ifNotExists: true) { table in
+                table.column(clothingId, primaryKey: true)
+                table.column(clothingName)
+                table.column(clothingType)
+                table.column(clothingColour)
+            })
+            
+        } catch {
+            print("could not create clothing table")
+        }
+    }
+    
+    func getClothing() {
+        do {
+            for clothing in try db!.prepare(clothingTable) {
+                print("id: \(clothing[clothingId]), name: \(clothing[clothingName]), colour: \(clothing[clothingColour]), type: \(clothing[clothingType]) ")
+            }
+        } catch {
+            print("failed to get cloting")
+        }
+    }
+    
+    func addClothing(name: String, colour: String, type: String) {
+        
+        do {
+            let insert = clothingTable.insert(clothingName <- name, clothingColour <- colour, clothingType <- type)
+            let rowid = try db!.run(insert)
+            // get rid of rowid if i don't need it
+        } catch {
+            print("cannot add clothing: \(name)")
+        }
     }
 
     func alive() {
